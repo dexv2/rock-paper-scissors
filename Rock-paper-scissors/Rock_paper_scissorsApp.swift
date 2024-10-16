e5112d47122bf5068ffeb95390752a1ae5c667d3
phil002109 <vermontphil.paguiligan@gmail.com>
2024-10-16 10:28:49 +0800

    Initial Commit

---
+++ Rock-paper-scissors/Rock_paper_scissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_paper_scissorsApp.swift
+//  Rock-paper-scissors
+//
+//  Created by Vermont Phil Paguiligan on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Rock_paper_scissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
